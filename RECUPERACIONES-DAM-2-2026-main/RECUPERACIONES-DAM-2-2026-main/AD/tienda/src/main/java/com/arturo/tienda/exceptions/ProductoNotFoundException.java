9f88bf2ce2fc74129732ba8f97cf19b19f27cb6f
arturobellidoblasco <abellidob@iesch.org>
2026-05-30 17:37:00 +0200

    commit 10

--- RECUPERACIONES-DAM-2-2026-main/RECUPERACIONES-DAM-2-2026-main/AD/tienda/src/main/java/com/arturo/tienda/exceptions/ProductoNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.arturo.tienda.exceptions;
-
-public class ProductoNotFoundException extends RuntimeException {
-    public ProductoNotFoundException(String message) {
-        super(message);
-    }
-}
--- RECUPERACIONES-DAM-2-2026-main/RECUPERACIONES-DAM-2-2026-main/AD/tienda/src/main/java/com/arturo/tienda/exceptions/ProductoYaExistenteException.java
+++
@@ -1,7 +0,0 @@
-package com.arturo.tienda.exceptions;
-
-public class ProductoYaExistenteException extends RuntimeException {
-    public ProductoYaExistenteException(String message) {
-        super(message);
-    }
-}
